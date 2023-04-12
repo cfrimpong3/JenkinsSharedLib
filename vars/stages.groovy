4e0efe4a9187f521b4734e93f0e9ed4908ae1b06
Ornel <126128620+cfrimpong3@users.noreply.github.com>
2023-04-12 14:18:53 -0600

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -2,8 +2,4 @@ def call(String Stage){
   if("${Stage}" == "package"){
   sh "${mavenHome}/bin/mvn package"
   } 
-}def call(String Stage){
-  if("${Stage}" == "package"){
-  sh "${mavenHome}/bin/mvn package"
-  } 
 }
